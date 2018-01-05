5e66dc9907b4c7d218ce6cae53c49cda478fff03
Lauren <alabele@gmail.com>
2018-01-05 12:32:41 -0600

    feat: App.js now filters books by current shelf
    Added a property to ListBooks that allows for books to be filtered to the correct shelf  according to their shelf property.

--- book-tracking/src/ListBooks.js
+++ book-tracking/src/ListBooks.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import ShelfForm from './ShelfForm'
+//import ShelfForm from './ShelfForm'
 //import {Link} from 'react-router-dom'
 //import PropTypes from 'prop-types'
 //import SortBy from 'sort-by'
@@ -14,24 +14,36 @@ class ListBooks extends Component {
 		query: ''
 	}
 
-	updateQuery = (query) => {
-		this.setState({query:query.trim()})
-	}
+	// updateQuery = (query) => {
+	// 	this.setState({query:query.trim()})
+	// }
+
 
 
 	render() {
 		const {books} = this.props
 		const {query} = this.state
+		const currentShelf = this.props.activeShelf
+		// function isCurrentlyReading(book) {
+		//   return book.shelf === 'currentlyReading';
+		// }
+		//let currentlyReadingShelf = books.map().filter(books.shelf === 'currentlyReading')
 		return (
 			<div className="bookshelf-books">
                 <ol className="books-grid">
-                {books.map((book) =>
+                {books.filter(book=>book.shelf===currentShelf).map((book) =>
                   <li key={book.id}>
                     <div className="book">
                       <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
                         <div className="book-shelf-changer">
-                          <ShelfForm/>
+                          <select value={this.state.shelf} onChange={this.handleChange}>
+					          <option value="none" disabled>Move to...</option>
+					          <option value="currentlyReading">Currently Reading</option>
+					          <option value="wantToRead">Want to Read</option>
+					          <option value="read">Read</option>
+					          <option value="none">None</option>
+					        </select>
                         </div>
                       </div>
                       <div className="book-title">{book.title}</div>
--- book-tracking/src/ShelfForm.js
+++ book-tracking/src/ShelfForm.js
@@ -3,32 +3,35 @@ import React, { Component } from 'react'
 class ShelfForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'coconut'};
-
+    this.state        = { shelf: '' } ;
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
+  }
+  // handleSubmit = (e) => {
+  //   e.preventDefault
+  //   if (this.props.onUpdateShelf)
+  //     this.props.onUpdateShelf(values)
+  // }
+
+  // handleChange(event) {
+  //   this.setState({value: event.target.value});
+  // }
+
+  // When select value is changed, update selected value
   handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('Your favorite flavor is: ' + this.state.value);
-    event.preventDefault();
+    this.setState({ shelf: event.currentTarget.value });
+    this.props.onUpdateShelf
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <select value={this.state.value} onChange={this.handleChange}>
+        <select value={this.state.shelf} onChange={this.handleChange}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
           <option value="none">None</option>
         </select>
-      </form>
     );
   }
 }
